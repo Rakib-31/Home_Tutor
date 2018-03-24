7312549feb66a79256ec10721490f3b05a338cc0
Rakib31 <30778508+Rakib31@users.noreply.github.com>
2018-03-24 13:02:30 +0600

    testing git

---
+++ app/src/main/java/com/example/asus_pc/home_tutor/EnglishActivity.java
@@ -0,0 +1,42 @@
+package com.example.asus_pc.home_tutor;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class EnglishActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button alphabet,vowel,consonant;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_english);
+
+        alphabet = (Button) findViewById(R.id.Alphabet);
+        vowel = (Button) findViewById(R.id.vowel);
+        consonant = (Button) findViewById(R.id.consonant);
+
+        alphabet.setOnClickListener(this);
+        vowel.setOnClickListener(this);
+        consonant.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.Alphabet){
+            Intent inten = new Intent(EnglishActivity.this, Alphabet.class);
+            startActivity(inten);
+        }
+        else if(v.getId() == R.id.vowel){
+            Intent inten = new Intent(EnglishActivity.this, Alphabet.class);
+            startActivity(inten);
+        }
+        else if(v.getId() == R.id.consonant){
+            Intent inten = new Intent(EnglishActivity.this, Alphabet.class);
+            startActivity(inten);
+        }
+    }
+}
--- app/src/main/java/com/example/asus_pc/home_tutor/MainActivity.java
+++ app/src/main/java/com/example/asus_pc/home_tutor/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.asus_pc.home_tutor;
 
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button bangla,english,number,write,math,draw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bangla = (Button) findViewById(R.id.bangla);
+        english = (Button) findViewById(R.id.english);
+        number = (Button) findViewById(R.id.number);
+        write = (Button) findViewById(R.id.write);
+        math = (Button) findViewById(R.id.math);
+        draw = (Button) findViewById(R.id.draw);
+
+        bangla.setOnClickListener(this);
+        english.setOnClickListener(this);
+        number.setOnClickListener(this);
+        write.setOnClickListener(this);
+        math.setOnClickListener(this);
+        draw.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v.getId() == R.id.bangla){
+            Intent inten = new Intent(MainActivity.this, EnglishActivity.class);
+            startActivity(inten);
+        }
+
+        else if(v.getId() == R.id.english){
+            Intent inten = new Intent(MainActivity.this, EnglishActivity.class);
+            startActivity(inten);
+        }
+
     }
 }
